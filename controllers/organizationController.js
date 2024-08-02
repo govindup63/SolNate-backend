a4a3d52ca3a09469cd94525f873f0388bee04db6
Govind <govindup63@gmail.com>
2024-08-03 03:47:45 +0530

    minor fixes

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -1,24 +1,27 @@
 const { generateDonationLink, generateQRCode } = require('../services/qrCodeService');
 const { sendEmail } = require('../services/emailService');
+const { getBalance } = require('../utils/solanaUtils'); // Import the getBalance function
 
 exports.createOrganization = async (req, res) => {
   try {
     const { publicKey, email } = req.body;
-
-    // Generate donation link and QR code
-    const donationLink = generateDonationLink(publicKey);
-    const qrCode = await generateQRCode(donationLink);
-
     // Ensure email is provided
     if (!email) {
       return res.status(400).json({ message: 'Email is required' });
     }
 
+    // Get balance of the provided public key
+    const balance = await getBalance(publicKey);
+    console.log(`Balance for public key ${publicKey}: ${balance}`);
+
+    // Generate donation link and QR code
+    const donationLink = generateDonationLink(publicKey);
+    const qrCode = await generateQRCode(donationLink);
     // Send confirmation email
     await sendEmail(email, donationLink);
     console.log(`Email sent to ${email} with public key ${publicKey} with donation link ${donationLink}`);
-    res.json({ donationLink, qrCode });
+    res.json({ donationLink, qrCode, balance });
   } catch (error) {
     res.status(500).json({ message: 'Error creating donation link', error });
   }
-};
+}
